d89d23225dd953a63e6d3acff56276719f0eac3a
ikbalghozali <ikbalthe@gmail.com>
2022-06-05 22:19:12 +0700

    connected to api

--- app/src/main/java/com/capstone/dressonme/ui/UserFragment.kt
+++ app/src/main/java/com/capstone/dressonme/ui/UserFragment.kt
@@ -1,26 +1,16 @@
 package com.capstone.dressonme.ui
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModelProvider
-import com.capstone.dressonme.R
 import com.capstone.dressonme.databinding.FragmentUserBinding
-import com.capstone.dressonme.local.UserPreference
 
-//private val Context(dataStore): DataStore<Preferences> by preferencesDataStore("settings")
 
 class UserFragment : Fragment() {
     private var _binding: FragmentUserBinding? = null
     private val binding get() = _binding!!
-    private lateinit var mainViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,16 +23,11 @@ class UserFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        mainViewModel = ViewModelProvider(
-//            this,
-//            ViewModelFactory(UserPreference.getInstance(dataStore))
-//        )[UserViewModel::class.java]
+
 
 
         binding.btnLogout.setOnClickListener {
-            mainViewModel.logout()
-            val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
-            fragmentTransition.replace(R.id.fragmentContainer, CameraFragment()).commit()
+
         }
     }
     companion object {
